bce46fa7f02c8560b48ecebd05f7b9a0e88ece0a
g-ilham <ilgamgaysin@gmail.com>
2016-11-17 17:52:42 +0300

    Setup

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+    mocks.verify_doubled_constant_names = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
